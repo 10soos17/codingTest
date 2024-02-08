1c689e9373d8a4a13d0ba9ccc1edcb381af993aa
10soos17 <shinsh1017@gmail.com>
2024-02-08 15:56:53 +0900

    [240208]위상정렬

---
+++ codingTest0116/newCoding1010/BJ_gold4_2056_작업_위상정렬.java
@@ -0,0 +1,27 @@
+package newCoding1010;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* https://www.acmicpc.net/problem/2056
+7
+5 0
+1 1 1
+3 1 2
+6 1 1
+1 2 2 4
+8 2 2 4
+4 3 3 5 6 
+
+출
+23
+*/
+public class BJ_gold4_2056_작업_위상정렬 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+
+	}
+}
