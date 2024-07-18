8ce08e6ea05b961c82b22d1b7e93ed5f5059fc0a
10soos17 <shinsh1017@gmail.com>
2024-07-18 17:45:30 +0900

    240718 실패

--- codingTest0116/src/newCoding1010/Programmers_가장많이받은선물.java
+++ codingTest0116/src/newCoding1010/Programmers_가장많이받은선물.java
@@ -21,13 +21,10 @@ friends	gifts	result
 public class Programmers_가장많이받은선물 {
 
 	static Map<String, Map<String, Integer>> give = new HashMap<>();
-	static Map<String, Map<String, Integer>> take = new HashMap<>();
 	static Map<String, Integer> profit = new HashMap<>();
-
 	public static void main(String[] args) throws Exception {
-		String[] friends = { "muzi", "ryan", "frodo", "neo" };
-		String[] gifts = { "muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi",
-				"frodo ryan", "neo muzi" };
+		String[] friends = {"joy", "brad", "alessandro", "conan", "david"};
+		String[] gifts = {"alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"};
 
 		int result = solution(friends, gifts);
 		System.out.println(result);
@@ -36,75 +33,58 @@ public class Programmers_가장많이받은선물 {
 	public static int solution(String[] friends, String[] gifts) {
 		int answer = 0;
 
-		Map<String, Integer> nameMap = new HashMap<>();
-		for (int i = 0; i < friends.length; i++) {
-			nameMap.put(friends[i], 0);
-		};
-
 		give = new HashMap<>();
-		take = new HashMap<>();
-
 		profit = new HashMap<>(); // 이름 나올떄마다
 
 		for (int i = 0; i < friends.length; i++) {
-			give.put(friends[i], nameMap);
-			take.put(friends[i], nameMap);
+			Map<String, Integer> map = new HashMap<>();
+			for (int j = 0; j < friends.length; j++) {
+				map.put(friends[j], 0);
+			}
+			give.put(friends[i], map);
 		}
 
 		for (int i = 0; i < gifts.length; i++) {
 			String giver = gifts[i].split(" ")[0];
 			String taker = gifts[i].split(" ")[1];
-			// map.put("key", map.getOrDefault("key", 0) + value);
 			// 준경우
 			Map<String, Integer> map = give.get(giver);
 			Integer count = map.get(taker) + 1;
 			map.put(taker, count);
 			give.put(giver, map);
-			profit.put(giver, profit.getOrDefault(giver, 1) + 1);
-			//System.out.println("giver:"+giver);
-			//System.out.println("gcnt:"+profit.get(giver));
-			
-			//받은경우
-			Map<String, Integer> map2 = take.get(taker);
-			count = map2.get(giver) + 1;
-			map2.put(giver, count);
-			take.put(taker, map2);
-			profit.put(taker, profit.getOrDefault(taker, -1) -1);
-			//System.out.println("taker:"+taker);
-			//System.out.println("tcnt:"+profit.get(taker));
+			profit.put(giver, profit.getOrDefault(giver, 0) + 1);
 		}
 		//  둘사이 주고받은 게 있으면, 많이 준 사람이 +1 / 기록 없거나 같으면 선물지수 큰 사람 +1 / 선물지수도 같으면 +0
 		for (int i = 0; i < friends.length; i++) {
+			
 			Integer n = 0;
 			String name = friends[i];
 			Map<String, Integer> gMap = give.get(name);
 			
 			for(String s : gMap.keySet()) {
+				
+				if(s.equals(name)) continue;
+				
 				Integer gCnt = gMap.get(s);
-				Map<String, Integer> tMap = take.get(s);
-				Integer tCnt = tMap.get(name);
-				if(gCnt != 0 && tCnt != 0) {
-					// 둘 값 비교
-					System.out.println("!=0"+":"+name+gCnt+":"+tCnt);
-					if(gCnt > tCnt) {
-						n+=1;
-						//System.out.println("if(gCnt > tCnt) {");
-					}
-				}
+				
+				Map<String, Integer> sMap = give.get(s);
+				Integer tCnt = sMap.get(name);
+				
+				if((gCnt != 0 || tCnt != 0) && gCnt > tCnt) n+=1;
 				else{
 					Integer pgCnt = profit.get(name);
 					Integer ptCnt = profit.get(s);
-					System.out.println(pgCnt);
-					System.out.println(ptCnt);
-					
-					if(pgCnt> ptCnt) {
-						n+=1;
-						//System.out.println("if(pgCnt> ptCnt) {");
+					if(pgCnt == null && ptCnt == null) continue;
+					else if(pgCnt == null && ptCnt != null) {
+						if(ptCnt < 0 ) n+=1;
 					}
+					else if(pgCnt != null && ptCnt == null) {
+						if(pgCnt > 0 ) n+=1;
+					}
+					else if(pgCnt> ptCnt) n+=1;
 				}
 			}
 			answer = Math.max(n, answer);
-			System.out.println("____ans:"+answer);
 		}
 		return answer;
 	}
